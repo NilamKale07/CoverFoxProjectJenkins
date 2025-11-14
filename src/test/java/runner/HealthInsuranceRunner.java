5624ba4f6c1a95a43619186d8effb8dbebd4f723
NilamKale07 <bvit.satishkale@gmail.com>
2025-11-14 22:44:59 +0530

    changed

--- src/test/java/runner/HealthInsuranceRunner.java
+++ src/test/java/runner/HealthInsuranceRunner.java
@@ -12,8 +12,8 @@ import io.cucumber.testng.CucumberOptions;
 		},
 
 		glue = { "stepDefinitions","hooks" }, 
-		plugin = {"pretty","html:Cucumber-reports/reports.html",
-				"json:Cucumber-reports/reports.json"}, 
+		plugin = {"pretty","html:target/Cucumber-reports/reports.html",
+				"json:target/Cucumber-reports/reports.json"}, 
 		publish = true,
 		dryRun=false)
 
